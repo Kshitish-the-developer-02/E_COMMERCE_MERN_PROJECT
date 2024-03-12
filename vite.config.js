65a21957d26d5eb9be966b334b5b36d14073622d
Kshitish-the-developer-02 <kshitishnayak82@gmail.com>
2024-03-12 11:10:46 +0530

    endpoint changes

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      "/api/":"http://localhost:5000",
-      "/uploads/":"http://localhost:5000"
+      "/api/":"https://college-project-f8n6.onrender.com",
+      "/uploads/":"https://college-project-f8n6.onrender.com"
     }
   }
 })
