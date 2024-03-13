fe0ee2c85b232627b4408e312a0755a52e033ec3
Kshitish-the-developer-02 <kshitishnayak82@gmail.com>
2024-03-13 09:46:08 +0530

    march 13 first commit

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      // "/api/":"https://college-project-f8n6.onrender.com",
-      // "/uploads/":"https://college-project-f8n6.onrender.com"
+      "/api/":"https://college-project-f8n6.onrender.com",
+      "/uploads/":"https://college-project-f8n6.onrender.com"
     }
   }
 })
